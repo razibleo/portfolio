a81a230b41f6453861dd7aaabf3c457c98bbd5d1
Razib <razibsarkerleo@gmail.com>
2022-11-12 02:20:46 +0400

    added new assets

---
+++ src/utils/assets.tsx
@@ -0,0 +1,99 @@
+import AndroidIcon from "../assets/logos/android.png";
+import AngularIcon from "../assets/logos/angular.png";
+import AwsIcon from "../assets/logos/aws.png";
+import CloudflareIcon from "../assets/logos/cloudflare.png";
+import Css3Icon from "../assets/logos/css3.png";
+import DockerIcon from "../assets/logos/docker.png";
+import ExpressIcon from "../assets/logos/express.png";
+import FirebaseIcon from "../assets/logos/firebase.png";
+import FlaskIcon from "../assets/logos/flask.png";
+import FlutterIcon from "../assets/logos/flutter.png";
+import GcpIcon from "../assets/logos/gcp.png";
+import GitIcon from "../assets/logos/git.png";
+import GooglecloudPlatformIcon from "../assets/logos/google_cloud_platform.png";
+import GoogleMapsIcon from "../assets/logos/google_maps.png";
+
+import GraphQLIcon from "../assets/logos/graphql.png";
+import Html5Icon from "../assets/logos/html5.png";
+
+import JavaIcon from "../assets/logos/java.png";
+import JavaFXIcon from "../assets/logos/javafx.png";
+import JavascriptIcon from "../assets/logos/javascript.png";
+import JQueryIcon from "../assets/logos/jquery.png";
+import IonicIcon from "../assets/logos/ionic.png";
+
+import LaravelIcon from "../assets/logos/laravel.png";
+import MatlabIcon from "../assets/logos/matlab.png";
+import MongoDBIcon from "../assets/logos/mongodb.png";
+
+import MySqlIcon from "../assets/logos/mysql.png";
+import NodejsIcon from "../assets/logos/nodejs.png";
+import OpenCVIcon from "../assets/logos/opencv.png";
+import PhpIcon from "../assets/logos/php.png";
+import PushNotificationsIcon from "../assets/logos/pushnotifications.png";
+import RaspberryPiIcon from "../assets/logos/raspberrypi.png";
+import PythonIcon from "../assets/logos/python.png";
+import ReactIcon from "../assets/logos/react.png";
+import ReduxIcon from "../assets/logos/redux.png";
+import RosIcon from "../assets/logos/ros.png";
+import TypescriptIcon from "../assets/logos/typescript.png";
+
+///images
+import AndroidTicTacToeHome from "../assets/projectimages/tictactoe_home-min.png";
+import AndroidTicTacToeGameplay from "../assets/projectimages/tictactoe_gameplay-min.png";
+import TheaterBooking from "../assets/projectimages/theater-min.gif";
+import Frogger from "../assets/projectimages/frogger-min.gif";
+import SpaceInvaders from "../assets/projectimages/space_invaders-min.gif";
+import CommuniserveHome from "../assets/projectimages/communiserve_home-min.png";
+import CommuniserveMaps from "../assets/projectimages/communiserve_maps-min.png";
+
+class Assets {
+  static readonly ANDROID_ICON: string = AndroidIcon;
+  static readonly ANGULAR_ICON: string = AngularIcon;
+  static readonly AWS_ICON: string = AwsIcon;
+  static readonly CLOUDFLARE_ICON: string = CloudflareIcon;
+  static readonly CSS3_ICON: string = Css3Icon;
+  static readonly CSS_ICON: string = AndroidIcon;
+  static readonly DOCKER_ICON: string = DockerIcon;
+  static readonly EXPRESS_ICON: string = ExpressIcon;
+  static readonly FIREBASE_ICON: string = FirebaseIcon;
+  static readonly FLASK: string = FlaskIcon;
+  static readonly FLUTTER_ICON: string = FlutterIcon;
+  static readonly GCP_ICON: string = GcpIcon;
+  static readonly GIT_ICON: string = GitIcon;
+  static readonly GOOGLECLOUD_PLATFORM_ICON: string = GooglecloudPlatformIcon;
+  static readonly Google_MAPS_ICON: string = GoogleMapsIcon;
+
+  static readonly GRAPHQL_ICON: string = GraphQLIcon;
+  static readonly HTML5_ICON: string = Html5Icon;
+  static readonly JAVA_ICON: string = JavaIcon;
+  static readonly JAVAFX_ICON: string = JavaFXIcon;
+  static readonly JAVASCRIPT_ICON: string = JavascriptIcon;
+  static readonly JQUERY_ICON: string = JQueryIcon;
+  static readonly IONIC_ICON: string = IonicIcon;
+  static readonly LARAVEL_ICON: string = LaravelIcon;
+  static readonly MATLAB_ICON: string = MatlabIcon;
+  static readonly MONGODB_ICON: string = MongoDBIcon;
+  static readonly MYSQL_ICON: string = MySqlIcon;
+  static readonly NODEJS_icon: string = NodejsIcon;
+  static readonly OPENCV_ICON: string = OpenCVIcon;
+  static readonly PHP_ICON: string = PhpIcon;
+  static readonly PUSH_NOTIFICATIONS_ICON: string = PushNotificationsIcon;
+  static readonly PYTHON_ICON: string = PythonIcon;
+  static readonly RASPBERRY_PI__ICON: string = RaspberryPiIcon;
+  static readonly REACT_ICON: string = ReactIcon;
+  static readonly REDUX_ICON: string = ReduxIcon;
+  static readonly ROS_ICON: string = RosIcon;
+  static readonly TYPSCRIPT_ICON: string = TypescriptIcon;
+
+  ////project images
+  static readonly TIC_TAC_TOE_HOME: string = AndroidTicTacToeHome;
+  static readonly TIC_TAC_TOE_GAMEPLAY: string = AndroidTicTacToeGameplay;
+  static readonly THEATER_GIF: string = TheaterBooking;
+  static readonly FROGGER_GIF: string = Frogger;
+  static readonly SPACEINVADERS_GIF: string = SpaceInvaders;
+  static readonly COUMMUNISERVE_HOME: string = CommuniserveHome;
+  static readonly COUMMUNISERVE_MAPS: string = CommuniserveMaps;
+}
+
+export default Assets;
