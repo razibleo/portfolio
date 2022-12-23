73804e4f3cfb5a98a8480ad70c6cc2105e03e28a
Razib <razibsarkerleo@gmail.com>
2022-12-24 03:11:50 +0400

    completed contact section

--- src/pages/home/contact/contact.tsx
+++ src/pages/home/contact/contact.tsx
@@ -1,6 +1,6 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { FC, FormEvent, useState } from "react";
+import { FC, useRef, useState } from "react";
 import SectionHeader from "../../../shared/components/sectionheader/sectionheader";
 import styles from "./contact.module.scss";
 import { primaryColor, secondaryColor } from "../../../theme/colors";
@@ -8,6 +8,11 @@ import { Element } from "react-scroll";
 import * as yup from "yup";
 import { ValidationError } from "yup";
 import parseYupError from "../../../utils/yupErrorParser";
+import { CircularProgress, Fade, alpha } from "@mui/material";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import FutureStatus from "../../../utils/FutureStatus";
+import { sleep } from "../../../utils/common_functions";
 
 const schema = yup.object().shape({
   email: yup
@@ -22,7 +27,17 @@ const schema = yup.object().shape({
 // type ContactData = InferType<typeof schema>;
 const Contact: FC = () => {
   const [formErrors, setFormErrors] = useState<any>({});
-  console.log("schema.fields", schema.type);
+  const containerRef = useRef<HTMLFormElement>(null);
+  const helloRef = useRef<HTMLDivElement>(null);
+  const goodbyeRef = useRef<HTMLDivElement>(null);
+  const [futureStatus, setFutureStatus] = useState<FutureStatus>(
+    FutureStatus.initialized
+  );
+
+  const isSuccess = futureStatus === FutureStatus.success;
+  const isLoading = futureStatus === FutureStatus.loading;
+  const nodeRef = isSuccess ? goodbyeRef : helloRef;
+
   const emailStyle = {
     backgroundColor: "#3b4353",
     marginBottom: "1.5%",
@@ -62,10 +77,11 @@ const Contact: FC = () => {
     <Element name="contact" className={styles["contact-form-wrapper"]}>
       <SectionHeader title={"Let's Chat"} />
       <form
+        ref={containerRef}
         className={styles["contact-form"]}
         // onSubmit={submitForm}
         // action="https://formspree.io/mvolplar"
-        onSubmit={(e: React.SyntheticEvent) => {
+        onSubmit={async (e: React.SyntheticEvent) => {
           e.preventDefault();
 
           const target = e.target as typeof e.target & {
@@ -81,6 +97,10 @@ const Contact: FC = () => {
               },
               { abortEarly: false, strict: true }
             );
+            setFormErrors({});
+            setFutureStatus((val) => FutureStatus.loading);
+            await sleep(2000);
+            setFutureStatus((val) => FutureStatus.success);
           } catch (e: any) {
             if (e instanceof ValidationError) {
               console.log(parseYupError(e));
@@ -111,7 +131,9 @@ const Contact: FC = () => {
             variant="filled"
           />
           {formErrors.email != null && (
-            <p className={styles.errormessage}>{formErrors.email[0]}</p>
+            <Fade in={formErrors.email != null} timeout={700}>
+              <p className={styles.errormessage}>{formErrors.email[0]}</p>
+            </Fade>
           )}
         </div>
 
@@ -130,24 +152,78 @@ const Contact: FC = () => {
             error={formErrors.message != null}
             // helperText={formErrors.message && formErrors.message[0]}
           />
+
           {formErrors.message != null && (
-            <p className={styles.errormessage}>{formErrors.message[0]}</p>
+            <Fade in={formErrors.message != null} timeout={700}>
+              <p className={styles.errormessage}>{formErrors.message[0]}</p>
+            </Fade>
           )}
         </div>
         <div className={styles["submit-wrapper"]}>
-          <Button
-            className={styles.submit}
-            sx={{
-              backgroundColor: secondaryColor,
-              "&:hover": {
-                backgroundColor: primaryColor,
-              },
-            }}
-            type="submit"
-            variant="contained"
-          >
-            Submit
-          </Button>
+          <SwitchTransition>
+            <CSSTransition
+              key={isSuccess ? "contactsuccess" : "contactfailure"}
+              nodeRef={nodeRef}
+              addEndListener={(done) => {
+                nodeRef.current!.addEventListener("transitionend", done, false);
+              }}
+              classNames={{
+                // appear: 'my-appear',
+                // appearActive: 'my-active-appear',
+                // appearDone: 'my-done-appear',
+                enter: styles["fade-enter"],
+                enterActive: styles["fade-enter-active"],
+                // enterDone: "my-done-enter",
+                exit: styles["fade-exit"],
+                exitActive: styles["fade-exit-active"],
+                // exitDone: "my-done-exit",
+              }}
+            >
+              <div ref={nodeRef}>
+                {isSuccess ? (
+                  <div
+                    className={styles["request-success-message"]}
+                    style={{ height: 50 }}
+                  >
+                    <CheckCircleOutlineIcon style={{ color: primaryColor }} />
+                    <div style={{ width: "8px" }} />
+                    <p>
+                      You request has been sent successfully.
+                      {/* I'll get to you as soon as
+                      possbile */}
+                    </p>
+                  </div>
+                ) : (
+                  <Button
+                    className={styles.submit}
+                    disabled={isLoading}
+                    sx={{
+                      backgroundColor: secondaryColor,
+                      "&:hover": {
+                        backgroundColor: primaryColor,
+                      },
+                      "&.Mui-disabled": {
+                        backgroundColor: alpha(primaryColor, 0.5),
+                      },
+                    }}
+                    style={{ height: 50 }}
+                    type="submit"
+                    variant="contained"
+                  >
+                    {isLoading ? (
+                      <CircularProgress
+                        sx={{ color: "white" }}
+                        style={{ width: 30, height: 30 }}
+                      />
+                    ) : (
+                      "Submit"
+                    )}
+                  </Button>
+                )}
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
+
           {formErrors.unknown != null && (
             <p className={styles.errormessage}>{formErrors.unknown[0]}</p>
           )}
